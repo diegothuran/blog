362dd8a04c3d5bb0ecf422b0e674f5a84080a8be
diegothuran <diego.thuran@gmail.com>
2019-03-27 20:04:28 -0300

    change article model

--- articles/models.py
+++ articles/models.py
@@ -22,9 +22,8 @@ class Article(models.Model):
     slug = models.SlugField(blank=True)
     body = models.TextField(blank=True)
     abstract = models.TextField(blank=True)
-#     date = models.DateTimeField(auto_now_add=True)
     date = models.DateTimeField()
-    thumb = models.ImageField(default="", blank=True)
+    thumb = models.TextField(blank=True)
     author = models.ForeignKey(User, default=None, on_delete=models.PROTECT)
     link = models.CharField(max_length=250, blank=True)
     
