bdc9fb20c3f0b5d30872c0ccb76802442ecee601
victorlorena <victor.lorena@gmail.com>
2019-05-17 10:26:41 -0300

    mexendo no profile e ajeitando outros html

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,5 @@
+# coding: utf-8
+
 from django.db import models
 from django.contrib.auth.models import User
 from django.db.models.signals import post_save
@@ -9,7 +11,10 @@ from django.db.models.signals import post_save
 
 class UserProfile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    categorias = models.ManyToManyField('category.Category')
+#     categorias = models.ManyToManyField('category.Category')
+    categorias = models.ManyToManyField('category.Category', help_text='Mantenha pressionado o "Control", ou "Command" no Mac, para selecionar mais de uma opção.')
+    
+    
 
     def __str__(self):
         return self.user.username
